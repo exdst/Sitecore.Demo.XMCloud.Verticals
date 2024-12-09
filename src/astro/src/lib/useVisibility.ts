1803b917b0c7a1e9ed1d86e0431fa92c951b7db5
Vadym Shcherban <vs@exdst.com.ua>
2024-12-09 18:01:16 +0200

    #31 Refactor and fix PromoCta component

--- src/astro/src/lib/useVisibility.ts
+++ src/astro/src/lib/useVisibility.ts
@@ -1,17 +1,17 @@
-export default function useVisibility(dom: Element, setVisible:(x: boolean, d: Element) => boolean, delay = 0) {
-    let isVisible = false;
+export default function useVisibility(dom: Element, setVisible: (x: boolean, d: Element) => boolean, delay = 0) {
+  let isVisible = false;
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                setTimeout(() => isVisible = setVisible(true, dom), delay);
-            } else {
-                isVisible = setVisible(false, dom);
-            }
-        });
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => isVisible = setVisible(true, dom), delay);
+      } else {
+        isVisible = setVisible(false, dom);
+      }
     });
-    if (dom) {
-        observer.observe(dom);
-    }
-    return isVisible;
+  });
+  if (dom) {
+    observer.observe(dom);
+  }
+  return isVisible;
 }
