ebe2fb7ea85b879a7ad5080e7032c902c39766fb
Antonytm <antontishchenko@gmail.com>
2025-01-14 14:51:02 +0200

    Minor changes for Edge configuration generation

--- src/astro/scripts/config/plugins/index.ts
+++ src/astro/scripts/config/plugins/index.ts
@@ -1,4 +1,4 @@
 export { packageJsonPlugin } from "./01_package-json";
 export { configPlugin } from "./02_config-plugin";
-export { fallbackPlugin } from "./03_fallback-plugin";
-export { edgePlatformPlugin } from "./04_edge-platform";
+export { edgePlatformPlugin } from "./03_edge-platform";
+export { fallbackPlugin } from "./10_fallback-plugin";
