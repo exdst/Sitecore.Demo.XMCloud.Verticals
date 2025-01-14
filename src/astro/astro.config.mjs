93744b0ad4e230e00ef83d5a08825774d67d1668
Antonytm <antontishchenko@gmail.com>
2025-01-14 18:23:05 +0200

    Universal configuration for Vercel/Node.js

--- src/astro/astro.config.mjs
+++ src/astro/astro.config.mjs
@@ -1,22 +1,27 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+
+const adapter = process.env.VERCEL ?
+vercel({
+  isr: {
+    // 5 minutes
+    expiration: 60 * 5,
+  },
+}) : node({
+  mode: 'standalone',
+});
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-  ],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  outDir: './dist',
+  integrations: [react()],
   security: {
     checkOrigin: false,
   },
-  i18n: {
-    locales: ["en", "fr-CA", "ja-JP"],
-    defaultLocale: "en",
-  }
+  server: {
+    port: 3000,
+  },
+  output: "server",
+  adapter: adapter,
 });
