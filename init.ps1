b4101c3f03c529208b28d3b4b4139f329b5f074d
Bogdan Druziuk <bdruziuk@gmail.com>
2025-01-27 16:59:39 +0200

    improve regex for adding hostnames

--- init.ps1
+++ init.ps1
@@ -100,7 +100,7 @@ finally {
 ################################
 
 $envContent = Get-Content .env -Encoding UTF8
-$hostnames = $envContent | Select-String -Pattern '(RENDERING_HOST_?\d?|CM_HOST|RENDERING_HOST)=(.+)'
+$hostnames = $envContent | Select-String -Pattern '(RENDERING_HOST_?\d?|CM_HOST)=(.+)'
 $hostnamesList = $hostnames.Matches.Groups | Where-Object { $_ -notmatch 'CM_HOST|RENDERING_HOST'}
 
 Write-Host "Adding Windows hosts file entries..." -ForegroundColor Green
