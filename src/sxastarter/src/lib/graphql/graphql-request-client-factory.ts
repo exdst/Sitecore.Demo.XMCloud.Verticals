216dd76b43fe6f7e0b15004fc55c335d217ae766
Vadym Shcherban <vs@exdst.com.ua>
2025-09-16 14:48:53 +0300

    Remove unused lib files

--- src/sxastarter/src/lib/graphql/graphql-request-client-factory.ts
+++
@@ -1,8 +0,0 @@
-import type { Debugger } from "@sitecore-jss/sitecore-jss";
-import { createGraphQLClientFactory } from "./graphql-create-client-factory";
-
-export default class GraphQLRequestClientFactory {
-  create(debug: Debugger) {
-    return createGraphQLClientFactory()();
-  }
-}
