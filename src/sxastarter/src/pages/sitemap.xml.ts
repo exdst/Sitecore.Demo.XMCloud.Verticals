151c66980bbb1abe24e9f3ddbfdc7da4f628a890
Vadym Shcherban <vs@exdst.com.ua>
2025-09-19 12:24:45 +0300

    Add Robots and Sitemap routes

---
+++ src/sxastarter/src/pages/sitemap.xml.ts
@@ -0,0 +1,17 @@
+import { APIContext, APIRoute } from "astro";
+import { SitemapMiddleware } from "@astro-sitecore-jss/astro-content-sdk/middleware";
+import scClient from "lib/sitecore-client";
+import sites from ".sitecore/sites.json";
+
+/**
+ * API route for generating sitemap.xml
+ *
+ * This API route dynamically generates and serves the sitemap XML for your site.
+ * The sitemap configuration can be managed within XM Cloud.
+ */
+
+export const GET: APIRoute = async ({ request }: APIContext) => {
+  // Wire up the SitemapMiddleware handler
+  const handler = new SitemapMiddleware(scClient, sites).getHandler();
+  return await handler(request);
+};
